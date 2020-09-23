0e2bc891edef7a8c262b3a92e5d31343eb0ae4b2
Shakespeare89 <growingupcollegestyle@gmail.com>
2020-09-22 21:15:26 -0600

    heroku test

--- src/components/screens/Category.js
+++ src/components/screens/Category.js
@@ -1,8 +1,8 @@
 import React from "react";
 import Header from "../parts/Header";
-import Breadcrumb from "../parts/Breadcrumb";
+
 import Posts from "../parts/Posts";
-import Sidebar from "../parts/Sidebar";
+
 import Footer from "../parts/Footer";
 
 const Category = () => {
--- src/server/routes/register.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const passport = require('passport')
-
-router.post('/', passport.authenticate('localRegister', {
-	successRedirect: '/account'
-}))
-
-module.exports = router
